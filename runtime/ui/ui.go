84cbc91e863806e60cb391e05876eb48a6da9c26
Alex Hussi <ahussi@users.noreply.github.com>
2026-04-13 22:21:01 +0800

    docs: add TODO note in Teardown for future context-based shutdown

--- runtime/ui/ui.go
+++ runtime/ui/ui.go
@@ -35,6 +35,7 @@ func (u *UI) Run() error {
 // Teardown cleans up any resources used by the UI.
 // Note: always safe to call even if Run() was never invoked.
 // This is deferred in main, so it will always execute on exit.
+// TODO: consider adding a context-based shutdown signal here in the future.
 func (u *UI) Teardown() {
 	if u.controller != nil {
 		u.controller.Teardown()
